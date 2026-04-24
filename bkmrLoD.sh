0e470b7d8e18fc12589ed0b4c6dc95928f0d7e0f
JordanAronOEHHA <jordan.aron@OEHHA.ca.gov>
2026-04-24 10:41:48 -0700

    refined scripts

--- bkmrLoD.sh
+++ bkmrLoD.sh
@@ -5,17 +5,17 @@
 #SBATCH --mail-type=NONE
 #SBATCH --mail-user=aron0064@umn.edu
 #SBATCH -A mfiecas
-#SBATCH -o /panfs/jay/groups/29/mfiecas/aron0064/bkmrLoD/LogFiles/%A_%a.out
-#SBATCH -e /panfs/jay/groups/29/mfiecas/aron0064/bkmrLoD/LogFiles/%A_%a.err
+#SBATCH -o /projects/standard/mfiecas/aron0064/bkmrLoDsim/LogFiles/%A_%a.out
+#SBATCH -e /projects/standard/mfiecas/aron0064/bkmrLoDsim/LogFiles/%A_%a.err
 n=$1
 lod_quantile=$2
 exposure_dist=$3
 h_dist=$4
 date
-path="$HOME/bkmrLoDsim"
+path="/projects/standard/mfiecas/aron0064/bkmrLoDsim"
 cd $path/Routputs
 start_time=$(date +%s)
-module load R/4.4.0
+module load R/4.4.0-openblas-rocky8
 Rscript $path/Rcode/bmkrAug.R $n $lod_quantile $exposure_dist $h_dist
 finish_time=$(date +%s)
 elapsed_time=$((finish_time  - start_time))
--- bkmrLoop.sh
+++ bkmrLoop.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -l
-
-for size in 300 600; do
-    if [[ $size == "300" ]]; then
-        time_limit="3:00:00"
-        mem_gb="5"
+for size in 400 800; do
+    if [[ $size == "400" ]]; then
+        time_limit="1:30:00"
+        mem_gb="2"
     else
         time_limit="6:00:00"
-        mem_gb="10"
+        mem_gb="2"
     fi
 
     for lod_quantile in 0.25 0.5 0.75; do
-        for exposure_dist in lnorm unif; do
+        for exposure_dist in gamma lnorm unif; do
             for h_dist in linear nonlinear; do
                 sbatch --time="$time_limit" --mem="${mem_gb}gb" bkmrLoD.sh $size $lod_quantile $exposure_dist $h_dist
             done
