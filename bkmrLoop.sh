479f00b4cdea52293fc4e3670b9576f9c8a86467
JordanAronOEHHA <jordan.aron@OEHHA.ca.gov>
2026-04-28 16:34:16 -0700

    new lod scheme

--- bkmrLoop.sh
+++ bkmrLoop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
-for size in 400 800; do
+for size in 800; do
     if [[ $size == "400" ]]; then
-        time_limit="5:00:00"
+        time_limit="3:00:00"
         mem_gb="3"
     else
         time_limit="16:00:00"
         mem_gb="3"
     fi
 
-    for lod_quantile in 0.25 0.5 0.75; do
+    for lod_quantile in 0.2 0.4 0.6 0.8; do
         for exposure_dist in gamma lnorm unif; do
             for h_dist in linear nonlinear; do
                 sbatch --time="$time_limit" --mem="${mem_gb}gb" bkmrLoD.sh $size $lod_quantile $exposure_dist $h_dist
